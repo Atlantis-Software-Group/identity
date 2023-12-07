1c951d7a75edf824212120980831ea0cd3cb53b9
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-07 11:48:47 -0500

    Initial setup

---
+++ src/asg.identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asg.identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
