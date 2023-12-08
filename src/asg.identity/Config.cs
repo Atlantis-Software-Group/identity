5157cbfedd31032fbe045f60e60135ae96ec4287
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-08 00:16:24 -0500

    Update Config.cs
    renamed interactive client
    renamed first scope to simple-budget-api
    removed second scope

--- src/asg.identity/Config.cs
+++ src/asg.identity/Config.cs
@@ -14,29 +14,16 @@ namespace asg.identity
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
+                new ApiScope("simple-budget-api"),
             };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
             {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
                 // interactive client using code flow + pkce
                 new Client
                 {
-                    ClientId = "interactive",
+                    ClientId = "backend-for-frontend",
                     ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
@@ -46,7 +33,7 @@ namespace asg.identity
                     PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    AllowedScopes = { "openid", "profile", "simple-budget-api" }
                 },
             };
     }
