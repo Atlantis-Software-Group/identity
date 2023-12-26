843c729fc6f04dd28a602e8f7572a22a3d01327e
ShahSwapnil <SwapnilShah99@gmail.com>
2023-12-25 21:12:31 -0500

    Update HostingExtensions.cs
    Remove code that is no longer needed.

--- src/asg.identity/HostingExtensions.cs
+++ src/asg.identity/HostingExtensions.cs
@@ -58,42 +58,21 @@ namespace asg.identity
                 .AddAspNetIdentity<ApplicationUser>()
                 .AddProfileService<ASGIdentityProfileService>();
 
-            builder.Services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-
-                    // register your IdentityServer with Google at https://console.developers.google.com
-                    // enable the Google+ API
-                    // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
-                });
-
+            builder.Services.AddAuthentication();
 
             // this adds the necessary config for the simple admin/config pages
-            {
-                builder.Services.AddAuthorization(options =>
-                    options.AddPolicy("admin",
-                        policy => policy.RequireClaim("email", "AliceSmith@email.com", "BobSmith@email.com"))
-                );
+            builder.Services.AddAuthorization(options =>
+                options.AddPolicy("admin",
+                    policy => policy.RequireClaim("email", "AliceSmith@email.com", "BobSmith@email.com"))
+            );
 
-                builder.Services.Configure<RazorPagesOptions>(options =>
-                    options.Conventions.AuthorizeFolder("/Admin", "admin"));
+            builder.Services.Configure<RazorPagesOptions>(options =>
+                options.Conventions.AuthorizeFolder("/Admin", "admin"));
 
-                builder.Services.AddTransient<asg.identity.Pages.Portal.ClientRepository>();
+            builder.Services.AddTransient<asg.identity.Pages.Portal.ClientRepository>();
             builder.Services.AddTransient<ClientRepository>();
             builder.Services.AddTransient<IdentityScopeRepository>();
-                builder.Services.AddTransient<ApiScopeRepository>();
-            }
-
-            // if you want to use server-side sessions: https://blog.duendesoftware.com/posts/20220406_session_management/
-            // then enable it
-            //isBuilder.AddServerSideSessions();
-            //
-            // and put some authorization on the admin/management pages using the same policy created above
-            //builder.Services.Configure<RazorPagesOptions>(options =>
-            //    options.Conventions.AuthorizeFolder("/ServerSideSessions", "admin"));
+            builder.Services.AddTransient<ApiScopeRepository>();
 
             return builder.Build();
         }
