71185b27d1b06b60e1a26d8e34b0dea1f4c8a927
ShahSwapnil <SwapnilShah99@gmail.com>
2024-01-01 17:48:39 -0500

    clean up
    remove entrypoint.sh files
    Remove config.cs
    Remove SeedData.cs

--- src/asg.identity.data.migrator/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd src
-cd asg.identity.data.migrator
-dotnet build ./asg.identity.data.migrator.csproj
-dotnet ./bin/Debug/net7.0/asg.identity.data.migrator.dll -migrate
--- src/asg.identity/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dotnet restore ./src/asg.identity/asg.identity.csproj
-dotnet build ./src/asg.identity/asg.identity.csproj
-
-cd src
-cd asg.identity
-dotnet watch ./bin/Debug/net7.0/asg.identity.dll
